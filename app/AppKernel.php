a1e4c96a2b0cfdab20194debd0077816b8f2cb25
beerfleet <jvbiervliet@gmail.com>
2017-07-20 23:51:18 +0200

    added FrontpageController

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            
+            // mijn Bundles
+            new \FrontpageBundle\FrontpageBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
